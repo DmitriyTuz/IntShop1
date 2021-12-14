13659452b10024eabb952b0a9cc8496542b0a744
ADMIN-1KMOQEUUB <dmitriytuz123@gmail.com>
2021-12-14 02:09:03 +0200

    refactor chat 10

--- server/Chat/user_functions.js
+++ server/Chat/user_functions.js
@@ -62,6 +62,8 @@ const removeUser = (id) => {
 
 const getUser = (id) => users.find((user) => user.id === id);
 
-const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+const getUsersInRoom = async (room) => User.findAll()
+
+// const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
 module.exports = { addUser, removeUser, getUser, getUsersInRoom };
