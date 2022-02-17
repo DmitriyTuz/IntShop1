d37b1e29a29c3a713aa1c9e349d54c372e8cffea
ADMIN-1KMOQEUUB <dmitriytuz123@gmail.com>
2022-02-17 16:09:08 +0200

    refactor chat 43 end

--- server/Chat/user_functions.js
+++ server/Chat/user_functions.js
@@ -3,35 +3,37 @@ const ApiError = require("../error/ApiError");
 
 const users = [];
 
-const addUser = async ({ email, name, room }) => {
+const addUser = async ({ socketId, email, name, room }) => {
 
+/*    try {*/
 
         name = name.trim().toLowerCase();
         room = room.trim().toLowerCase();
         email = email.trim().toLowerCase();
 
-        let user1 = await User.findOne( { where: { email } } );
-//        console.log("*** user1 = ", user1);
 
-        if ( !user1 ) return { error: 'User with this email not found' };
+        let user1 = await User.update({ socketId }, { where: { email } } )
 
-/*        if ( !user1 ) {
-            user1 = await User.create( {name: name, email: email} )
-        };*/
+        let user2 = await User.findOne( { where: { email } } );
+
+//*        if ( !user2 ) throw { message: 'User with this email not found' };
+
+        if ( !user2 ) return { error: 'User with this email not found' };
+
 
         let room1 = await Room.findOne( { where: { name: room } } );
         if ( !room1 ) {
             room1 = await Room.create( { name: room } )
         }
 
-//        console.log('*** UserRoom = ', UserRoom);
-        let existingUser1 = await UserRoom.findOne( { where: { userId: user1.id, roomId: room1.id } } );
+
+        let existingUser1 = await UserRoom.findOne( { where: { userId: user2.id, roomId: room1.id } } );
 
         if(!email || !name || !room) return { error: 'Username, email and room are required.' };
 
         if(existingUser1) return { error: 'Username is taken.' };
 
-        existingUser1 = await UserRoom.create( {userId: user1.id, roomId: room1.id} );
+        existingUser1 = await UserRoom.create( {userId: user2.id, roomId: room1.id} );
 
 //        const existingUser = users.find((user) => user.room === room && user.name === name);
 
@@ -43,15 +45,28 @@ const addUser = async ({ email, name, room }) => {
 
 //        const user = { id, user1.name, email, room };
 
-        let id = user1.id;
-            name = user1.name;
-        const user = { id, name, email, room };
+        let id = user2.id;
+            name = user2.name;
+
+        const user = { id, socketId, name, email, room };
 
        users.push(user);
 
         return { user };
 
+/*    } catch (e) {
+        console.log(1);
+//        throw (e);
+        console.log('Ошибка catch 1 : ', e.message);
+
+//        console.log('Ошибка : ', e.name);
+//        return { error: e };
+
+//        if(!email || !name || !room) return { error: 'Username, email and room are required.' };
+
+    }*/
 }
+
 
 const removeUser = async (id) => {
     await UserRoom.destroy({
@@ -67,23 +82,17 @@ const removeUser = async (id) => {
     if(index !== -1) return users.splice(index, 1)[0];
 }*/
 
-const getUser = async (Id, room) => {
+const getUser = async (room, socketId) => {
 
-    let user1 = await User.findOne( { where: { id: Id } } );
-
-//    let room1 = await UserRoom.findOne( { where: { userId: Id } } );
-//    console.log('***room1 = ', room1);
+    let user1 = await User.findOne( { where: { socketId: socketId } } );
 
     let id = user1.id;
-    console.log(id);
+//    console.log(id);
 
     let name = user1.name;
     let email = user1.email;
 
-//    let room1 = room1.roomId;
-//    console.log('***room = ', room1);
-
-    const user = { id, name, email, room };
+    const user = { id, socketId, name, email, room };
 
     return user;
 }
--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,25 +1,34 @@
 const { User, Room, Message, Basket, BasketDevice, Device} = require('../models/models')
 const { addUser, getUsersInRoom, getUser, removeUser } = require('../Chat/user_functions');
 const ApiError = require("../error/ApiError");
+const {where} = require("sequelize");
+
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 let userId;
 
 class ChatController {
 
     async getChat(req, res, next) {
-        const { email } = req.body
-        if (!email) {
-            return next(ApiError.badRequest('Некорректный email'))
+        const { message } = req.query;
+        if (!message) {
+            return next(ApiError.badRequest('Введите сообщение : '))
         }
-        let chat = await User.findAll({attributes: [],
-            include: [{
-                model: Room, attributes:[],
-                required: false,
+
+//        const user = await User.findAll({ where: { email: { [Op.like]: `%${req.query.email}%` } } })
+
+        let chat = await User.findAll({attributes: ["id", "socketId", "name"],
+
                 include: [{
+
                     model: Message, attributes:["id","text"],
-                    required: false
+                    required: true,
+                    where : { text : { [Op.like]: `%${req.query.message}%` } }
+
+//                    where : { text : message }
+
                 }]
-            }]
         })
         return res.json(chat)
     };
@@ -32,14 +41,40 @@ class ChatController {
 
             socket.on('join', async ({ email, name, room }, callback) => {
 
+/*                try {*/
+                    const { error, user } = await addUser({ socketId: socket.id, email, name, room });
+                    console.log('***user= ', user);
+
+                    if(error) return callback(error);
+
+                    userId = user.id;
+
+
+                    socket.join(user.room);
+
+                    socket.emit('message', { userId: user.id, roomName: user.room, user: 'admin', text: `${user.name}, welcome to ${user.room}.`});
+                    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+                    let users = await getUsersInRoom(user.room);
+                    console.log('***users= ' , users);
+
+                    io.to(user.room).emit('roomData', { room: user.room, users: users });
+
+                    callback();
+                /*} catch (e) {
+                    console.log(2);
+                    console.log('Ошибка catch 2 : ', e.message);
+                    return callback(e.message);
+                };*/
+
 
 //                console.log('Принимаем на сервере имя и комнату из события join созданного на клиенте  !', name, room);
 
-                const { error, user } = await addUser({ email, name, room });
+/*                const { error, user } = await addUser({ socketId: socket.id, email, name, room });
                 console.log('***user= ', user);
 
 
-                if(error) return callback(error);
+                if(error) return callback(error.message);
 
                 userId = user.id;
 
@@ -54,7 +89,7 @@ class ChatController {
 
                 io.to(user.room).emit('roomData', { room: user.room, users: users });
 
-                callback();
+                callback();*/
             });
 
             socket.on('sendMessage', async (message, callback) => {
@@ -64,15 +99,22 @@ class ChatController {
 
 //                const user = getUser(socket.id);
 
-                const user = await getUser(message.id, message.room);
+                const user = await getUser(message.room, socket.id);
+
+//                const user = await getUser(message.id, message.room, socket.id);
+
+//                const user = await getUser(socket.id);
+
                 console.log('***user = ', user);
 
                 io.to(user.room).emit('message', { userId: message.id, roomName: user.room, user: user.name, text: message.message });
 
                 console.log('=====>*** ', message.message, message.id, message.room);
 
-//                const message1 = await Message.create({text: message.message, userId: message.id /*roomId: message.room*/});
-//                console.log('***message1 = ', message1);
+                const room1 = await Room.findOne ( { where : { name : user.room } } );
+
+                const message1 = await Message.create({text: message.message, userId: message.id, roomId: room1.id});
+                console.log('***message1 = ', message1);
 
                 callback();
             });
