a1d48297e0a5f916fc3f4a4b11a0f53451a80bec
DmitriyTuz <dmitriytuz123@gmail.com>
2022-05-08 17:34:15 +0300

    IntShop with migrations 3

--- server/migrations/20220320185301-migration-create-all-users.js
+++ server/migrations/20220320185301-migration-create-all-users.js
@@ -11,34 +11,34 @@ const Schema = mongoose.Schema;
 module.exports = {
   async up (queryInterface, Sequelize) {
 
-      let users = await userM.find();
-      let baskets = await Baskets.find();
+      // let users = await userM.find();
+      // let baskets = await Baskets.find();
 
     // console.log('***user = ', users);
     // console.log('***user = ', users[0].name);
     // console.log('***userLength = ', users.length);
 
-      for (let i = 0; i <= users.length - 1; i++) {
-          await userM.deleteOne({name: users[i].name});
-      }
+       // for (let i = 0; i <= users.length - 1; i++) {
+       //     await userM.deleteOne({name: users[i].name});
+       // }
+       //
+       // for (let i = 0; i <= baskets.length - 1; i++) {
+       //     await Baskets.deleteOne({name: baskets[i].name});
+       // }
 
-      for (let i = 0; i <= baskets.length - 1; i++) {
-          await Baskets.deleteOne({name: baskets[i].name});
-      }
+      // await User.create({ name: 'Dima', email: 'dim@mail.ru', password: '111'});
+      // await User.create({ name: 'Alexey', email: 'al@mail.ru', password: '222'});
 
-      /*    await User.create({ name: 'Dima', email: 'dim@mail.ru', password: '111'});
-      await User.create({ name: 'Alexey', email: 'al@mail.ru', password: '222'});*/
-
-      /*    let user = await userM.find();
-
-          console.log('***user = ', user);
-          console.log('***user = ', user[0].name);
-          console.log('***userLength = ', user.length);
-
-          for ( let i = 0; i <= user.length - 1; i++ ) {
-            await User.create({ name: user[i].name, email: user[i].email } );
-
-          }*/
+          // let user = await userM.find();
+          //
+          // console.log('***user = ', user);
+          // console.log('***user = ', user[0].name);
+          // console.log('***userLength = ', user.length);
+          //
+          // for ( let i = 0; i <= user.length - 1; i++ ) {
+          //   await User.create({ name: user[i].name, email: user[i].email } );
+          //
+          // }
      },
 
 
--- zadanie/readme.txt
+++
@@ -1,9 +0,0 @@
-                        1. HEROKU
-
-1) Migrations:
-                # запуск миграций - heroku run npx sequelize-cli db:migrate;
-
-                        2. GITHUB
-
-1) Если проблема с пушем:
-                            # проблемка с пушем в новый репазиторий - git remote set-url origin https://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@github.com/globalluxsoft/instantly-clicked.git/
